b47acacac263680231123c78713ff67cfada3ea5
shishaofeng <18839966905@163.com>
2022-04-22 12:40:54 +0800

    精简代码

--- src/components/SearchList/index.jsx
+++ src/components/SearchList/index.jsx
@@ -2,33 +2,36 @@ import { useState } from "react";
 import { nanoid } from "nanoid";
 import "./index.css";
 function SearchList() {
+
   let [list, setList] = useState([]);
-  const keyUp = (e) => {
-    if (e.keyCode === 13 && e.target.value) {
-      setList([{ text: e.target.value, id: nanoid() }, ...list]);
-    }
-  };
+
   // 直接使用 需要return 一个方法
-  const deleteCur1 = (id) => {
+  const deleteCur = (id) => {
     return () => {
       setList(list.filter((item) => item.id !== id));
     };
   };
-  // 箭头函数用法
-  //   const deleteCur2 = (id) => {
-  //     setList(list.filter((item) => item.id !== id));
-  //   };
   return (
     <div>
+      <h1>列表页--useState--版本</h1>
       <div className="search">
-        <input type="text" name="" id="" onKeyUp={keyUp} />
+        <input
+          type="text"
+          name=""
+          id=""
+          onKeyUp={(e) => {
+            if (e.key === "Enter" && e.target.value) {
+              setList([{ text: e.target.value, id: nanoid() }, ...list]);
+            }
+          }}
+        />
       </div>
+
       <ul className="search-list">
         {list.map((item) => {
           return (
             <li key={item.id}>
-              <button onClick={deleteCur1(item.id)}>删除</button>
-              {/* <button onClick={() => deleteCur2(item.id)}>删除</button> */}
+              <button onClick={deleteCur(item.id)}>删除</button> &nbsp;
               {item.text}
             </li>
           );
